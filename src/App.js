4538e2fd77ce78a93c5ed567254d42375716e72c
Prasun-Acharjee <acharjee.prasun@gmail.com>
2019-08-18 12:03:47 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {Route} from "react-router-dom";
 import {Switch} from "react-router-dom";
 import Home from "./home";
-import Delhi from "./delhi";
-import Kolkata from "./kolkata";
+import Child from "./Child";
+//import Delhi from "./delhi";
+//import Kolkata from "./kolkata";
 //import {withRouter} from "react-router-dom";
+
 class App extends React.Component
 {
   render()
@@ -12,8 +14,7 @@ class App extends React.Component
     return(<div>
     <Switch>
       <Route exact path="/" component={Home}/>
-      <Route path="/kolkata" component={Kolkata}/>
-      <Route path="/delhi" component={Delhi}/>
+      <Route path="/:id" component={Child}/>
       </Switch>
       </div>
       
--- src/home.js
+++ src/home.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import {withRouter} from "react-router-dom";
+import {withRouter, NavLink ,Link} from "react-router-dom";
 class Home extends React.Component
 {
     render()
     {
         return(
         <div>
-            
+            <h1>Cities</h1>
             <button onClick={()=>this.props.history.push("/kolkata")}>Kolkata</button>
             <button onClick={()=>this.props.history.push("/delhi")}>Delhi</button>
+            <Link to="/kolkata">Kolkata(link)</Link>
+            <NavLink to="/delhi" style={{ background:'yellow', color:'white' }}>Delhi(NavLink)</NavLink>
         </div>);
     }
 }
